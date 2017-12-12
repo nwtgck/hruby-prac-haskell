a434f548a0f827124f2057220eadab1861551aaf
Ryo Ota <nwtgck@gmail.com>
2017-12-12 23:43:37 +0900

    [add] Add an example for Ruby array

--- app/Main.hs
+++ app/Main.hs
@@ -2,19 +2,34 @@ module Main where
 
 import Foreign.Ruby
 
-printRubyIntOne :: IO ()
-printRubyIntOne = 
-    withRubyInterpreter $ \ri -> do
-        -- Haskell => Ruby
-        Right rubyOne <- toRuby ri (1 :: Int)
-        putStr "Haskell => Ruby `1`: "
-        print rubyOne
+printRubyIntOne :: RubyInterpreter -> IO ()
+printRubyIntOne ri = do
+    putStrLn "====== Int ======"
+    -- Haskell => Ruby
+    Right rubyOne <- toRuby ri (1 :: Int)
+    putStr "Haskell => Ruby: "
+    print rubyOne
 
-        -- Ruby => Haskell
-        Right haskellOne <- fromRuby ri rubyOne :: IO (Either RubyError Int)
-        putStr "Ruby => Haskell `1`:"
-        print haskellOne
+    -- Ruby => Haskell
+    Right haskellOne <- fromRuby ri rubyOne :: IO (Either RubyError Int) -- NOTE: type notation is necessary
+    putStr "Ruby => Haskell:"
+    print haskellOne
+
+printRubyIntArray :: RubyInterpreter -> IO ()
+printRubyIntArray ri = do
+    putStrLn "====== Array Int ======"    
+    -- Haskell => Ruby
+    Right rubyArr <- toRuby ri ([5, -3, 93, -73, 2] :: [Int])
+    putStr "Haskell => Ruby: "
+    print rubyArr
+
+    -- Ruby => Haskell
+    Right haskellList <- fromRuby ri rubyArr :: IO (Either RubyError [Int]) -- NOTE: type notation is necessary
+    putStr "Ruby => Haskell:"
+    print haskellList
 
 main :: IO ()
 main = do
-    printRubyIntOne
+    withRubyInterpreter $ \ri -> do
+        printRubyIntOne ri
+        printRubyIntArray ri
