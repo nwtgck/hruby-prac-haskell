ec209bd760fde4ecd7b5d00e763e87e7708a489a
Ryo Ota <nwtgck@gmail.com>
2017-12-12 23:25:54 +0900

    [add] Add an example of "Ruby => Haskell"

--- app/Main.hs
+++ app/Main.hs
@@ -2,9 +2,22 @@ module Main where
 
 import Foreign.Ruby
 
+printRubyIntOne :: IO ()
+printRubyIntOne = do
+    ri            <- startRubyInterpreter
+
+    -- Haskell => Ruby
+    Right rubyOne <- toRuby ri (1 :: Int)
+    putStr "Haskell => Ruby `1`: "
+    print rubyOne
+
+    -- Ruby => Haskell
+    Right haskellOne <- fromRuby ri rubyOne :: IO (Either RubyError Int)
+    putStr "Ruby => Haskell `1`:"
+    print haskellOne
+
+    closeRubyInterpreter ri
+
 main :: IO ()
 main = do
-    ri            <- startRubyInterpreter
-    Right rubyOne <- toRuby ri (1 :: Int)
-    print rubyOne
-    closeRubyInterpreter ri
+    printRubyIntOne
