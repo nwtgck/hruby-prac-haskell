5082cd5cb8b689e7266591c4996bb8e4526f71a2
Ryo Ota <nwtgck@gmail.com>
2017-12-12 23:27:40 +0900

    [refactor] Use withRubyInterpreter instead of startRubyInterpreter and closeRubyInterpreter

--- app/Main.hs
+++ app/Main.hs
@@ -3,20 +3,17 @@ module Main where
 import Foreign.Ruby
 
 printRubyIntOne :: IO ()
-printRubyIntOne = do
-    ri            <- startRubyInterpreter
+printRubyIntOne = 
+    withRubyInterpreter $ \ri -> do
+        -- Haskell => Ruby
+        Right rubyOne <- toRuby ri (1 :: Int)
+        putStr "Haskell => Ruby `1`: "
+        print rubyOne
 
-    -- Haskell => Ruby
-    Right rubyOne <- toRuby ri (1 :: Int)
-    putStr "Haskell => Ruby `1`: "
-    print rubyOne
-
-    -- Ruby => Haskell
-    Right haskellOne <- fromRuby ri rubyOne :: IO (Either RubyError Int)
-    putStr "Ruby => Haskell `1`:"
-    print haskellOne
-
-    closeRubyInterpreter ri
+        -- Ruby => Haskell
+        Right haskellOne <- fromRuby ri rubyOne :: IO (Either RubyError Int)
+        putStr "Ruby => Haskell `1`:"
+        print haskellOne
 
 main :: IO ()
 main = do
