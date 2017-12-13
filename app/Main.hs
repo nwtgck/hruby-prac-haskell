7d99851c82ba312d9c31757db56c26719158a7b6
Ryo Ota <nwtgck@gmail.com>
2017-12-13 19:53:05 +0900

    [add] Add a practice of method call (messy)

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,8 @@ module Main where
 import Foreign.Ruby
 import Foreign.Ruby.Bindings
 
+import Foreign.Ptr
+
 printRubyIntOne :: RubyInterpreter -> IO ()
 printRubyIntOne ri = do
     putStrLn "====== Int ======"
@@ -87,7 +89,66 @@ doMethodCall3 ri = do
     putStr "rubyArr * 3: "
     Right haskellTimesedArr <- fromRuby ri rubyTimesed :: IO (Either RubyError [Int])
     print haskellTimesedArr
-    
+
+defFunc :: RubyInterpreter -> IO ()
+defFunc ri = do
+  putStrLn "====== Define a function ======"
+  let
+    func1 :: RValue -> IO RValue
+    func1 rbArr = do
+      Right rbThree <- toRuby ri (3 :: Int)
+      -- call Array#*
+      Right rbTimesed  <- mySafeMethodCall ri rbArr "*" [rbThree]
+      return rbTimesed
+
+  a <- registerGlobalFunction1 ri "arryTimes3" func1
+--  b <- registerGlobalFunction1 ri "arryTimes3" func1
+  self <- rb_gv_get "itself"
+--  v    <- rb_gv_get "arrTimes3"
+  v    <- getSymbol "arryTimes3"
+
+
+--  print (nullPtr :: Ptr RValue)
+
+--  kernelClass <- rb_gv_get "class"
+
+
+
+  Right rbArr  <- toRuby ri ([1, 2, 3] :: [Int])
+
+--  res <- safeMethodCall ri "Object" "arrTimes3" [rbArr]
+
+--  res2 <- mySafeMethodCall ri v "call" [rbArr]
+
+  Right a2 <- safeMethodCall ri "Kernel" "method" [v]
+  mapRID <- rb_intern "map"
+
+  itselfS   <- getSymbol "next"
+
+
+  c <- c_rb_funcall_with_block rbArr mapRID 0 nullPtr itselfS
+
+  print a2
+  print c
+
+  Right hs <- fromRuby ri c :: IO (Either RubyError [Int])
+
+  print hs
+
+
+  res2 <- rtype a2
+  print res2
+
+--  print a
+----  print b
+--  print self
+--  print v
+--  print res2
+
+--  print kernelClass
+
+  return ()
+
 main :: IO ()
 main = do
     withRubyInterpreter $ \ri -> do
@@ -95,4 +156,5 @@ main = do
         printRubyIntArray ri
         doMethodCall1 ri
         doMethodCall2 ri
-        doMethodCall3 ri        
+        doMethodCall3 ri
+        defFunc ri
