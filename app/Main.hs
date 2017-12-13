cb856bd3ba69feb456e8e07bbc20668c00add1e0
Ryo Ota <nwtgck@gmail.com>
2017-12-13 19:56:51 +0900

    [change] Clean up defFunc

--- app/Main.hs
+++ app/Main.hs
@@ -93,59 +93,27 @@ doMethodCall3 ri = do
 defFunc :: RubyInterpreter -> IO ()
 defFunc ri = do
   putStrLn "====== Define a function ======"
-  let
-    func1 :: RValue -> IO RValue
-    func1 rbArr = do
-      Right rbThree <- toRuby ri (3 :: Int)
-      -- call Array#*
-      Right rbTimesed  <- mySafeMethodCall ri rbArr "*" [rbThree]
-      return rbTimesed
-
-  a <- registerGlobalFunction1 ri "arryTimes3" func1
---  b <- registerGlobalFunction1 ri "arryTimes3" func1
-  self <- rb_gv_get "itself"
---  v    <- rb_gv_get "arrTimes3"
-  v    <- getSymbol "arryTimes3"
-
-
---  print (nullPtr :: Ptr RValue)
-
---  kernelClass <- rb_gv_get "class"
-
+--  let
+--    func1 :: RValue -> IO RValue
+--    func1 rbArr = do
+--      Right rbThree <- toRuby ri (3 :: Int)
+--      -- call Array#*
+--      Right rbTimesed  <- mySafeMethodCall ri rbArr "*" [rbThree]
+--      return rbTimesed
+--
+--  registerGlobalFunction1 ri "arryTimes3" func1
+--  arrTimes3Sym    <- getSymbol "arryTimes3"
+--  Right arrTimes3Method <- safeMethodCall ri "Kernel" "method" [arrTimes3Sym]
 
 
   Right rbArr  <- toRuby ri ([1, 2, 3] :: [Int])
+  mapRID  <- rb_intern "map"
+  nextSym <- getSymbol "next"
 
---  res <- safeMethodCall ri "Object" "arrTimes3" [rbArr]
+  c <- c_rb_funcall_with_block rbArr mapRID 0 nullPtr nextSym
+  Right hsValue <- fromRuby ri c :: IO (Either RubyError [Int])
 
---  res2 <- mySafeMethodCall ri v "call" [rbArr]
-
-  Right a2 <- safeMethodCall ri "Kernel" "method" [v]
-  mapRID <- rb_intern "map"
-
-  itselfS   <- getSymbol "next"
-
-
-  c <- c_rb_funcall_with_block rbArr mapRID 0 nullPtr itselfS
-
-  print a2
-  print c
-
-  Right hs <- fromRuby ri c :: IO (Either RubyError [Int])
-
-  print hs
-
-
-  res2 <- rtype a2
-  print res2
-
---  print a
-----  print b
---  print self
---  print v
---  print res2
-
---  print kernelClass
+  print hsValue
 
   return ()
 
